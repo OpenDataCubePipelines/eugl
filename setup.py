91457a00b0c546aaa3cc775dc4bb3ac89af9b599
Imam Tashdid ul Alam <imam.alam@gmail.com>
2018-07-20 11:58:20 +1000

    v0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='eugl',
-      version='0.0.2',
+      version='0.0.3',
       description='Modules that deal with sensor and data quality characterisation.',
       packages=find_packages(),
       install_requires=[
