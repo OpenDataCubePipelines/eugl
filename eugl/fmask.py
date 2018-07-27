d7545128e4f0ca325b0d2287eda396b5d7c1aa39
Alex Vincent <2034948+ASVincent@users.noreply.github.com>
2018-07-27 14:47:25 +1000

    improving logging on subprocess commands

--- eugl/fmask.py
+++ eugl/fmask.py
@@ -8,12 +8,16 @@ by datacube.
 from __future__ import absolute_import
 import os
 from os.path import join as pjoin, abspath, basename, dirname, exists
-from subprocess import check_call
+import subprocess
 import tempfile
+import logging
+
 from pathlib import Path
 import click
 
 from wagl.acquisition import acquisitions
+
+_LOG = logging.getLogger(__name__)
 
 os.environ["CPL_ZIP_ENCODING"] = "UTF-8"
 
@@ -32,7 +36,23 @@ def run_command(command, work_dir):
     """
     A simple utility to execute a subprocess command.
     """
-    check_call(' '.join(command), shell=True, cwd=str(work_dir))
+    _proc = subprocess.Popen(
+        ' '.join(command),
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        shell=True,
+        cwd=str(work_dir)
+    )
+    _proc.wait()
+
+    stderr, stdout = _proc.communicate()
+    if _proc.returncode != 0:
+        _LOG.error(stderr.decode('utf-8'))
+        _LOG.info(stdout.decode('utf-8'))
+        raise subprocess.CalledProcessError('"%s" failed with return code: %s' % (command, str(_proc.returncode)))
+    else:
+        _LOG.debug(stdout.decode('utf-8'))
+
 
 
 def _fmask_landsat(acquisition, out_fname, work_dir):
@@ -46,7 +66,7 @@ def _fmask_landsat(acquisition, out_fname, work_dir):
             tmp_dir.mkdir()
         cmd = ['tar', 'zxvf', str(acquisition_path)]
         run_command(cmd, tmp_dir)
-        
+
         acquisition_path = tmp_dir
 
     # wild cards for the reflective bands
@@ -68,7 +88,7 @@ def _fmask_landsat(acquisition, out_fname, work_dir):
 
     reflective_bands = [str(path) for path in acquisition_path.rglob(
                         reflective_wcards[acquisition.platform_id])]
-    
+
     thermal_bands = [str(path) for path in acquisition_path.rglob(
                      thermal_wcards[acquisition.platform_id])]
 
--- eugl/gqa/geometric_utils.py
+++ eugl/gqa/geometric_utils.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Functions in this file represent work to be refactored.
+
 from __future__ import print_function
 import datetime
 import logging
