69af40fdb3e9dd41660ce57704b37b141c2d5a9b
Alex Vincent <2034948+ASVincent@users.noreply.github.com>
2018-10-23 12:31:59 +1100

    Update for travis sdist build

--- setup.py
+++ setup.py
@@ -4,22 +4,31 @@ from setuptools import setup, find_packages
 
 import versioneer
 
-setup(name='eugl',
-      version=versioneer.get_version(),
-      description='Modules that deal with sensor and data quality characterisation.',
-      packages=find_packages(),
-      install_requires=[
-          'eodatasets',
-          'click',
-          'click_datetime',
-          'numpy',
-          'rasterio',
-          'rios',
-          'python-fmask'
-      ],
-      package_data={'eugl.gqa': ['data/*.csv']},
-      dependency_links=[
-          'hg+https://bitbucket.org/chchrsc/rios/get/rios-1.4.5.zip#egg=rios-1.4.5',
-          'hg+https://bitbucket.org/chchrsc/python-fmask/get/python-fmask-0.4.5.zip#egg=python-fmask-0.4.5'
-      ]
+setup(
+    name='eugl',
+    description='Modules that deal with sensor and data quality characterisation.',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    url='https://github.com/OpenDataCubePipelines/eugl',
+    author='The wagl authors',
+    author_email='earth.observation@ga.gov.au',
+    maintainer='wagl developers',
+    packages=find_packages(),
+    install_requires=[
+        'eodatasets',
+        'click',
+        'click_datetime',
+        'numpy',
+        'rasterio',
+        'rios',
+        'python-fmask',
+        'wagl',
+    ],
+    package_data={'eugl.gqa': ['data/*.csv']},
+    dependency_links=[
+        'hg+https://bitbucket.org/chchrsc/rios/get/rios-1.4.5.zip#egg=rios-1.4.5',
+        'hg+https://bitbucket.org/chchrsc/python-fmask/get/python-fmask-0.4.5.zip#egg=python-fmask-0.4.5'
+        'git+https://github.com/GeoscienceAustralia/wagl@master#egg=wagl',
+        'git+https://github.com/GeoscienceAustralia/eo-datasets.git@develop#egg=eodatasets',
+    ]
 )
