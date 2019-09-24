ebf4557e6ac638d355d88f113ab6ab887059cdc9
Josh Sixsmith <josh.sixsmith@gmail.com>
2019-09-24 13:14:35 +1000

    Removed eodatasets from the list of dependencies.

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
     maintainer='wagl developers',
     packages=find_packages(),
     install_requires=[
-        'eodatasets',
         'click',
         'click_datetime',
         'numpy',
@@ -29,6 +28,5 @@ setup(
         'hg+https://bitbucket.org/chchrsc/rios/get/rios-1.4.5.zip#egg=rios-1.4.5',
         'hg+https://bitbucket.org/chchrsc/python-fmask/get/python-fmask-0.4.5.zip#egg=python-fmask-0.4.5'
         'git+https://github.com/GeoscienceAustralia/wagl@develop#egg=wagl',
-        'git+https://github.com/GeoscienceAustralia/eo-datasets.git@develop#egg=eodatasets',
     ]
 )
