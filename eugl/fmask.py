07976c0e9154ab903f5cb0939c5e12699d19c87e
Sai Ma <Sai.Ma@ga.gov.au>
2022-03-18 14:22:45 +1100

    fix the lint error

--- eugl/fmask.py
+++ eugl/fmask.py
@@ -300,14 +300,14 @@ def _sentinel2_fmask(
         "gdal_edit.py",
         "-ro",
         "-offset",
-        ' '.join([str(e) for e in offset_values]),
-        temp_vrt_fname
+        " ".join([str(e) for e in offset_values]),
+        temp_vrt_fname,
     ]
 
     run_command(cmd, work_dir)
 
     # use this CLI to apply offset metadata for the bands
-    cmd = ["gdal_translate",  temp_vrt_fname,  vrt_fname, "-unscale"]
+    cmd = ["gdal_translate", temp_vrt_fname, vrt_fname, "-unscale"]
 
     run_command(cmd, work_dir)
 
